5d7740c3331ea543c31544b94ac52a693a82bf21
acp <carvalhohuzumak@gmail.com>
2024-01-24 14:38:55 -0300

    finalizado testes

--- src/services/userCreateService.spec.js
+++ src/services/userCreateService.spec.js
@@ -1,20 +1,56 @@
 const UserCreateService = require("./userCreateService");
 const UserRepositoryInmemory = require("../repositories/userRepositoryInMemory");
+const AppError = require('../utils/AppError');
 
-it("must return an id", async () => {
 
-    const user = {
-        name: "Jhon Test",
-        email: 'jhon@gmail.com',
-        password: "123"
-    };
+describe("UserCreateService", () => {
+    let userRepositoryInmemory = null;
+    let userCreateService = null;
+
+
+    beforeEach(() => {
+        userRepositoryInmemory = new UserRepositoryInmemory();
+        userCreateService = new UserCreateService(userRepositoryInmemory)
+
+    });
     
-    const userRepositoryInmemory = new UserRepositoryInmemory();
-    const userCreateService = new UserCreateService(userRepositoryInmemory);
-    const userCreated = await userCreateService.execute(user);
+    it("must return an id", async () => {
+        const user = {
+            name: "Jhon Test",
+            email: 'jhon@gmail.com',
+            password: "123"
+        };
+        
+        const userCreated = await userCreateService.execute(user);
+        expect(userCreated).toHaveProperty("id");
 
-    console.log(userCreated)
+    });
+
+    it("must return an error with this email already exists at database", async () => {
+        const user1 = {
+            name: "Jhon Test",
+            email: 'jhon2@gmail.com',
+            password: "123"
+        };
+
+        const user2 = {
+            name: "Jhon Test",
+            email: 'jhon2@gmail.com',
+            password: "123"
+        };
+        
+        await userCreateService.execute(user1);
+
+        expect( async () => {
+            await userCreateService.execute(user2);
 
 
-    expect(userCreated).toHaveProperty("id");
+        }).rejects.toEqual(new AppError("Este E-mail já está cadastrado!!!"));
+
+    });
+
+
+
+
+    
 });
